3187ce0174ebe4fd1b4aac3c65bc82f55ea88ea6
Ivan Elias Carrasco Lupinta <ivaneliascalu@gmail.com>
2018-12-26 02:20:12 -0500

    Agregar archivos de proyecto.

---
+++ Image2Txt/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Image2Txt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.texto = new System.Windows.Forms.RichTextBox();
+            this.imagen = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // texto
+            // 
+            this.texto.Location = new System.Drawing.Point(697, 61);
+            this.texto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.texto.Name = "texto";
+            this.texto.ReadOnly = true;
+            this.texto.Size = new System.Drawing.Size(569, 637);
+            this.texto.TabIndex = 0;
+            this.texto.Text = "";
+            // 
+            // imagen
+            // 
+            this.imagen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imagen.Location = new System.Drawing.Point(34, 61);
+            this.imagen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imagen.Name = "imagen";
+            this.imagen.Size = new System.Drawing.Size(538, 637);
+            this.imagen.TabIndex = 1;
+            this.imagen.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccionar : ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Image2Txt.Properties.Resources.icons8_resume_32;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(411, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "  Abrir Imagen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::Image2Txt.Properties.Resources.icons8_opened_folder_32;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(254, 13);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "        Abrir Carpeta";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1279, 719);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imagen);
+            this.Controls.Add(this.texto);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Imagen A Texto";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.imagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox texto;
+        private System.Windows.Forms.PictureBox imagen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Image2Txt/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MODI;
+
+namespace Image2Txt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            texto.Text="";
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Abrir Archivo";
+            ofd.Filter = "Jpg files (*.jpg)|*.jpg|Gif files (*.gif)|*.gif|Bitmap files (*.Bmp)|*.bmp|PNG files (*.png)|*.png*";
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                string nombreArchivo = ofd.FileName;
+                Bitmap Picture = new Bitmap(nombreArchivo);
+
+                imagen.Image = (System.Drawing.Image)Picture;
+
+                imagen.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                String NombreImg = ofd.SafeFileName;
+
+                Document Mivariable = new Document();
+                Mivariable.Create(@nombreArchivo);
+                Mivariable.OCR(MODI.MiLANGUAGES.miLANG_SPANISH, true, true);
+                MODI.Image image2 = (MODI.Image)Mivariable.Images[0];
+                Layout layout = image2.Layout;
+
+                foreach (Word word in layout.Words)
+                {
+                    texto.Text = texto.Text + word.Text+" ";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string rutaCarpeta = "";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            
+            if (fbd.ShowDialog()==DialogResult.OK)
+            {
+                rutaCarpeta = fbd.SelectedPath;
+                IEnumerator archivos = Directory.GetFiles(rutaCarpeta).GetEnumerator();
+                while (archivos.MoveNext())
+                {
+                    string extension = Path.GetExtension(Convert.ToString(archivos.Current));
+                    string nombreArchivo = Convert.ToString(archivos.Current).Replace(extension, string.Empty);
+
+                    if (extension == ".jpg" || extension == ".JPG" || extension == ".bmp" ||
+                    extension == ".BMP" || extension == ".tif" || extension == ".TIF" ||
+                    extension == ".gif" || extension == ".GIF" || extension == ".png" ||
+                    extension == ".PNG" || extension == ".tiff" || extension == ".TIFF")
+                    {
+                        try
+                        {
+                            Document documento = new Document();
+                            documento.Create(Convert.ToString(archivos.Current));
+                            documento.OCR(MODI.MiLANGUAGES.miLANG_SPANISH, true, true);
+                            MODI.Image imagen2 = (MODI.Image)documento.Images[0];
+                            texto.Text = imagen2.Layout.Text;
+
+                            string ruta = nombreArchivo + extension;
+                            imagen.Image= new System.Drawing.Bitmap(ruta);
+                            imagen.SizeMode = PictureBoxSizeMode.StretchImage;
+                        }
+                        catch (Exception exc)
+                        {
+
+                            MessageBox.Show(exc.Message);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
